4c029da7fb5139868c0483462dbbd0ac4efafdd8
Norbert Parti <norbertparti07@gmail.com>
2023-01-13 09:16:06 +0000

    Switches plus random generation

--- switches.go
+++ switches.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
+)
+
+func calculateGrade(score int) string {
+	var grade string
+	switch {
+	case score < 60:
+		grade = "F"
+	case score < 70:
+		grade = "D"
+	case score < 80:
+		grade = "C"
+	case score < 90:
+		grade = "B"
+	default:
+		grade = "A"
+	}
+	return grade
+}
 
 func switch_example() {
-	fmt.Print("Enter a grade: ")
+	rand.Seed(time.Now().UnixNano())
+	point := rand.Intn(100)
+	grade := calculateGrade(point)
+	println(point, grade)
 }
